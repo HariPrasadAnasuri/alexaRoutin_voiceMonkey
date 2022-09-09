553f6bb0522ccac77159b1a79537baf8f310ff13
HariPrasadAnasuri <40918656+HariPrasadAnasuri@users.noreply.github.com>
2022-09-09 23:02:25 +0530

    1. There are some issues but working most of the functionality. Issue like the url announcement text saying test and all.

--- src/main/groovy/com/bvirtuoso/hari/scheduler/InvokeAlexaRoutineScheduler.groovy
+++ src/main/groovy/com/bvirtuoso/hari/scheduler/InvokeAlexaRoutineScheduler.groovy
@@ -3,6 +3,7 @@ package com.bvirtuoso.hari.scheduler
 import com.bvirtuoso.hari.api.ApiInvoker
 import com.bvirtuoso.hari.model.DishInfo
 import com.bvirtuoso.hari.model.HariSchedule
+import com.bvirtuoso.hari.model.OnOrOffDuration
 import com.bvirtuoso.hari.model.PersonInfo
 import com.bvirtuoso.hari.model.PersonalSchedule
 import com.bvirtuoso.hari.model.jpa.DishInfoJpa
@@ -14,8 +15,11 @@ import org.apache.juli.logging.Log
 import org.apache.juli.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
 import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
@@ -44,12 +48,11 @@ class InvokeAlexaRoutineScheduler {
     @Value("\${voiceMonkey.entertainment.turnOff}") String turnOffEntertainment
     @Value("\${voiceMonkey.entertainment.turnOn}") String turnOnEntertainment
     @Value("\${voiceMonkey.drinkWater}") String drinkWater
-    String wildcard
+    String hariAnnouncement
     @Value("\${app.bvirtuoso.turnOffTv}") String turnOffTv
     @Value("\${app.bvirtuoso.turnOnTv}") String turnOnTv
     @Value("\${voiceMonkey.tellTime}") String tellTime
     @Value("\${voiceMonkey.tellDate}") String tellDate
-    @Value("\${voiceMonkey.pleaseWalk}") String pleaseWalk
     @Value("\${app.bvirtuoso.onOffDuration}") String onOffDuration
     @Value("\${app.bvirtuoso.getPeopleInfo}") String getPeopleInfo
     @Value("\${app.bvirtuoso.getDishInfo}") String getDishInfo
@@ -57,24 +60,32 @@ class InvokeAlexaRoutineScheduler {
     @Value("\${app.bvirtuoso.harshaAvailability}") String harshaAvailability
     @Value("\${voiceMonkey.turnOnDecoLight}") String turnOnDecoLight
     @Value("\${voiceMonkey.turnOffDecoLight}") String turnOffDecoLight
+    @Value("\${voiceMonkey.hallAnnouncement}") String hallAnnouncement
+    @Value("\${voiceMonkey.entertainment.play}") String playTv
+    @Value("\${voiceMonkey.entertainment.pause}") String pauseTv
 
 
     private final DishInfoRepository dishInfoRepository
     private final HealthInfoRepository healthInfoRepository
 
-    private boolean isLightOn = false;
+    private boolean isLightOn = false
+    private boolean isPleaseWalkAnnounced = false
+    private LocalDateTime walkingStartedTime = LocalDateTime.now()
+    private Duration walkingInterval
+    private boolean justAnnouncedAboutWalking = false
+    private int warningCounterToSwitchOffTv = 0
 
     public InvokeAlexaRoutineScheduler(RestTemplate restTemplate, ApiInvoker apiInvoker,
                                         DishInfoRepository dishInfoRepository,
                                         HealthInfoRepository healthInfoRepository,
-                                       @Value("\${voiceMonkey.wildcard}") String wildcard){
+                                       @Value("\${voiceMonkey.hariAnnouncement}") String hariAnnouncement){
         log.debug("Scheduler initialized")
         this.restTemplate = restTemplate
         this.apiInvoker = apiInvoker
         this.dishInfoRepository = dishInfoRepository
         this.healthInfoRepository = healthInfoRepository
-        this.wildcard = wildcard
-        this.apiInvoker.invokeVoiceMonkeyApi(wildcard + "Hey hari, application is deployed")
+        this.hariAnnouncement = hariAnnouncement
+        this.apiInvoker.invokeVoiceMonkeyApi(hariAnnouncement + "Hey hari, application is deployed")
     }
 
     @Scheduled(cron = "10 0/20 10-23 * * *")
@@ -174,18 +185,60 @@ class InvokeAlexaRoutineScheduler {
         decoLightStatus.setLocalDateTime(LocalDateTime.now())
     }
 
-    @Scheduled(cron = "0/30 * * * * *")
+    @Scheduled(cron = "0/2 * * * * *")
     void harshaIdleCheck(){
         String availability = apiInvoker.invokeApi(harshaAvailability)
         if(availability.equals("available")){
             //Period period = Period.between(harshaExerciseStatus.getLocalDateTime(), LocalDateTime.now());
             Duration duration = Duration.between(harshaExerciseStatus.getLocalDateTime(), LocalDateTime.now())
-            if(duration.getSeconds() > 900){
+            if(duration.getSeconds() > 600){
                 log.debug("Announcing - Please Walk")
-                apiInvoker.invokeApi(pleaseWalk)
+                apiInvoker.invokeApi(hallAnnouncement+"Harsha please walk")
                 harshaExerciseStatus.setLocalDateTime(LocalDateTime.now())
+                isPleaseWalkAnnounced = true
+                justAnnouncedAboutWalking = false
             }else{
-                apiInvoker.invokeApi(turnOnDecoLight)
+                if(isPleaseWalkAnnounced){
+                    log.debug("Just announced please walk");
+                    walkingStartedTime = LocalDateTime.now()
+
+                    isPleaseWalkAnnounced = false
+                }else{
+                    if(duration.getSeconds() < 60) {
+                        walkingInterval = Duration.between(walkingStartedTime, LocalDateTime.now())
+                        //log.debug("Since ${walkingInterval.getSeconds()} you are walking");
+                    }else{
+                        if(!justAnnouncedAboutWalking) {
+                            justAnnouncedAboutWalking = true
+                            if (walkingInterval.getSeconds() > 120) {
+                                apiInvoker.invokeApi("${hallAnnouncement}That's awesome you've been walking since 1 minute")
+                                if(warningCounterToSwitchOffTv > 1){
+                                    //Turn on TV
+                                    log.debug("Turning on TV")
+                                    apiInvoker.invokeApi(turnOnEntertainment)
+                                    warningCounterToSwitchOffTv = 0
+                                }
+                            } else {
+
+                                apiInvoker.invokeApi("${hallAnnouncement}I didn't see you that you are walking. If this repeat next time I'll turn off tv")
+                                warningCounterToSwitchOffTv++
+
+                                if(warningCounterToSwitchOffTv > 1){
+                                    //pause TV
+                                    log.debug("Pause TV")
+                                    apiInvoker.invokeApi(pauseTv)
+                                }
+                                if(warningCounterToSwitchOffTv > 2){
+                                    //Turn off TV
+                                    log.debug("Turning off TV")
+                                    apiInvoker.invokeApi(turnOffEntertainment)
+                                }
+                            }
+                        }
+                    }
+                }
+                //log.debug("")
+                //apiInvoker.invokeApi(wildcard+ "you can sit now")
             }
         }
     }
@@ -210,19 +263,26 @@ class InvokeAlexaRoutineScheduler {
         }
     }
 
-    @Scheduled(cron = "0 0/1 15-17 * * *")
+    //@Scheduled(cron = "0 0/1 15-17 * * *")
     public void tvOnOrOff(){
         //log.debug("Announcing tvOnOff")
         //println("Inside tvOnOrOff method")
         //After setting user agent only able to call the API
         // Always check the IDE pointing proper Java Installation or not, otherwise will get trust store keys issue.
-        /*HttpHeaders headers = new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        HttpEntity<String> entity = new HttpEntity<>()<String>(headers);
         OnOrOffDuration onOrOffDuration = restTemplate.exchange(onOffDuration, HttpMethod.GET, entity, OnOrOffDuration.class).getBody()
         long minutes = onOrOffDuration.getDuration().toMinutes()
+
         if(onOrOffDuration.isTvOn()){
+            if(minutes > 10){
+                this.apiInvoker.invokeVoiceMonkeyApi(hariAnnouncement + "Arey yar please walk in between")
+            }
+        }
+
+        /*if(onOrOffDuration.isTvOn()){
             if(minutes > 120){
                 restTemplate.exchange(turnOffTv, HttpMethod.GET, entity, OnOrOffDuration.class).getBody()
             }
@@ -230,8 +290,8 @@ class InvokeAlexaRoutineScheduler {
             if(minutes > 30){
                 restTemplate.exchange(turnOnTv, HttpMethod.GET, entity, OnOrOffDuration.class).getBody()
             }
-        }
-        println("onOrOffDuration ${onOrOffDuration}")*/
+        }*/
+        println("onOrOffDuration ${onOrOffDuration}")
     }
 
     @Scheduled(cron = "0 0 2 * * *")
@@ -274,7 +334,7 @@ class InvokeAlexaRoutineScheduler {
                 if (schedule.getLocalDateTime().isBefore(LocalDateTime.now())) {
                     schedule.setAlreadyAdded(true)
                     log.debug("Alexa hari schedule call: "+schedule.getTaskDesc() + "about "+ (schedule.getDuration().getSeconds()/60) +"Minutes");
-                    apiInvoker.invokeVoiceMonkeyApi(wildcard + schedule.getTaskDesc() + "about "+ (schedule.getDuration().getSeconds()/60) +"Minutes")
+                    apiInvoker.invokeVoiceMonkeyApi(hariAnnouncement + schedule.getTaskDesc() + "about "+ (schedule.getDuration().getSeconds()/60) +"Minutes")
                 }
             }
         }
